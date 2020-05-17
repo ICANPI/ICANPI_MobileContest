7db9fa55f2fcdce1a9070cfcc8a08bf36a87ea9a
yechan0704 <key07040@naver.com>
2020-05-17 20:20:42 +0900

    Splash

--- app/src/main/java/com/example/danjam/Login.java
+++ app/src/main/java/com/example/danjam/Login.java
@@ -2,16 +2,13 @@ package com.example.danjam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, Splash.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_login);
     }
 }
