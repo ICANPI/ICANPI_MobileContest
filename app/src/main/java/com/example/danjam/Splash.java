3bf6f5f0ed40aba9b2dffa0ef0b748f6d51c074a
yechan0704 <key07040@naver.com>
2020-05-20 23:42:10 +0900

    custom_elevation

--- app/src/main/java/com/example/danjam/Splash.java
+++ app/src/main/java/com/example/danjam/Splash.java
@@ -24,4 +24,9 @@ public class Splash extends AppCompatActivity {
             }
         },2000);
     }
+    @Override
+    public void onBackPressed() {
+        //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
+    }
+
 }
